3981e84d15bc452c6795e99549fb62bc25b44dce
Spencer Bohol <zxtm89j1@gmail.com>
2022-08-04 18:08:11 +0800

    handling keypress event

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = () => {
-    console.log('Button clicked!');
+   
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
@@ -20,7 +20,18 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnsOpenModal[i].addEventListener('click', openModal)
 }
 
-btnCloseModal.addEventListener('click', closeModal)
+btnCloseModal.addEventListener('click', closeModal);
 
-overlay.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (e) => {
+   
+    // console.log(e)
+
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      
+            closeModal()
+        
+    }
+})
 
